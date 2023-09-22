3c9dd67cdeeb2637273cdc722740df47cf690430
luke1188 <49883653+luke1188@users.noreply.github.com>
2023-09-22 14:43:54 -0400

    get workspace hermes data

--- src/commands/build.rs
+++ src/commands/build.rs
@@ -1,5 +1,8 @@
 use anyhow::Result;
 use clap::Args;
+use std::env;
+use std::fs;
+use std::path::Path;
 use tracing::info;
 
 /// Build your application
@@ -18,20 +21,63 @@ impl BuildArgs {
 }
 
 pub fn build(args: &BuildArgs) -> Result<()> {
-    let file = include_str!("../../Cargo.toml");
+    // Get the current working directory as a String
+    let current_dir = env::current_dir()?;
+    let current_dir_str = current_dir.to_string_lossy().to_string();
+    let workspace_toml = format!("{}/Cargo.toml", current_dir_str);
+    info!("Current working directory: {:?}", current_dir_str);
+
+    // Check if the Cargo.toml file exists
+    if !Path::new(&workspace_toml).exists() {
+        return Err(anyhow::anyhow!(
+            "No Cargo.toml found in the current directory"
+        ));
+    }
+
+    let toml_content = fs::read_to_string(&workspace_toml)?;
+    // Find the Cargo.toml file
 
     #[derive(serde::Deserialize)]
     struct Package {
         version: String,
+        metadata: MetaData,
+    }
+
+    #[derive(serde::Deserialize)]
+    struct MetaData {
+        hermes: HermesMetaData,
     }
     #[derive(serde::Deserialize)]
-    struct MyToml {
+    struct HermesMetaData {
+        icon_android: String,
+        icon_iphone: String,
+        icon_web: String,
+        require_webgpu: bool,
+    }
+    #[derive(serde::Deserialize)]
+    struct WorkspaceToml {
         package: Package,
     }
-    let toml_file: MyToml = toml::from_str(file).unwrap();
+    let toml_file: WorkspaceToml = toml::from_str(&toml_content).unwrap();
     info!(
-        "The version of your Cargo.toml package is: {:?}",
+        "The version in your Cargo.toml package is: {:?}",
         args.version.clone().unwrap_or(toml_file.package.version)
+    );
+    info!(
+        "Found: {:?} as current favicon",
+        toml_file.package.metadata.hermes.icon_web
+    );
+    info!(
+        "Found: {:?} as current android icon",
+        toml_file.package.metadata.hermes.icon_android
+    );
+    info!(
+        "Found: {:?} as current iphone icon",
+        toml_file.package.metadata.hermes.icon_iphone
+    );
+    info!(
+        "Web GPU Enabled? {:?}",
+        toml_file.package.metadata.hermes.require_webgpu
     );
 
     // Exit cleanly
